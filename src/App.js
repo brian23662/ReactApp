ad11edd6abbe5dc3064e822d1f3606b0bba616ea
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2021-06-24 22:11:47 -0400

    hello react with search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
 function App() {
+  // you can do something in between
+  const title = 'React';
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
