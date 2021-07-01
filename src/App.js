84ae9ea5b141a0a6efd5ab7d6e8649eb64c47ce5
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2021-06-30 21:33:22 -0400

    search event handler

--- src/App.js
+++ src/App.js
@@ -18,23 +18,43 @@ const list = [
     objectID: 1,
   },
 ];
-function App() {
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+    <hr />
+
+    <List />
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+  };
+
   return (
     <div>
-      <h1>My Hacker Stories</h1>
-
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
-      <hr />
-
-      <ul>
-        {list.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
-        })}
-      </ul>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-}
+};
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
